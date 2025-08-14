1c0486489c1044ce606610c892f83f7efca91073
anikethsanthan <anikethsanthan999@gmail.com>
2025-08-14 14:57:52 +0530

    Added apis

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,6 +3,10 @@ export const apiBaseUrl = "http://localhost:7777";
 
 export const apiUrls = {
   auth: {
-    login: "/bunnybloom/user/login",
+    login: "/expense-tracker/user/login",
+  },
+  expenses: {
+    getexpenses: "/expense-tracker/transaction/store-transaction",
+    addexpenses: "/expense-tracker/transaction/get-transaction",
   },
 };
